11b67dc179904d3d10ce6a60e4858a0586f95427
Subaru Sakaguchi <subarusakaguchi@yahoo.com.br>
2023-10-21 12:21:04 -0300

    Table and chip added, still needs color correction and component separation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,79 @@
 import { Component } from '@angular/core';
+import { ObjectValues } from 'src/utils/objectValue';
+
+const POSSIBLE_TASK_STATUS = {
+  OPEN: 'Aberto',
+  CLOSED: 'Concluído',
+  EXPIRED: 'Expirado',
+} as const;
+
+const POSSIBLE_STATUS_COLOR = {
+  INFO: 'info',
+  SUCCESS: 'success',
+  WARN: 'warn',
+} as const;
+
+type TaskStatus = ObjectValues<typeof POSSIBLE_TASK_STATUS>;
+
+type TaskStatusColor = ObjectValues<typeof POSSIBLE_STATUS_COLOR>;
+
+export interface TaskRowInfo {
+  task: string;
+  cpf: string;
+  responsible: string;
+  dueDate: Date;
+  status: TaskStatus;
+  statusColor: TaskStatusColor;
+}
+
+const ELEMENT_DATA: TaskRowInfo[] = [
+  {
+    task: 'Primeira tarefa',
+    cpf: '123.456.789-00',
+    responsible: 'Subaru',
+    dueDate: new Date(),
+    status: 'Aberto',
+    statusColor: 'info',
+  },
+  {
+    task: 'Segunda tarefa',
+    cpf: '123.456.789-00',
+    responsible: 'Subaru',
+    dueDate: new Date(),
+    status: 'Concluído',
+    statusColor: 'success',
+  },
+  {
+    task: 'Terceira tarefa',
+    cpf: '123.456.789-00',
+    responsible: 'Subaru',
+    dueDate: new Date(),
+    status: 'Expirado',
+    statusColor: 'warn',
+  },
+  {
+    task: 'Quarta tarefa',
+    cpf: '123.456.789-00',
+    responsible: 'Subaru',
+    dueDate: new Date(),
+    status: 'Aberto',
+    statusColor: 'info',
+  },
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'to-do-list';
+  displayedColumns: string[] = [
+    'task',
+    'cpf',
+    'responsible',
+    'dueDate',
+    'status',
+  ];
+  dataSource = ELEMENT_DATA;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatChipsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
